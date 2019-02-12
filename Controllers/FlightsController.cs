9d7a6daf410b5c19c702fef439c0565ba37d686f
Manan Kharbanda <manankharbanda91@gmail.com>
2019-02-12 13:42:47 -0500

    Data and knowledge management project

---
+++ Controllers/FlightsController.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using FlightManagementSystem.Filters;
+using FlightManagementSystem.Models;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace FlightManagementSystem.Controllers
+{
+    public class FlightsController : Controller
+    {
+        //
+        // GET: /Flights/
+
+        public ActionResult MainPage()
+        {
+            return View();
+        }
+
+        public ActionResult AddFlight()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddFlight(AddFlights model)
+        {
+            MySqlConnection conn = null;
+            string myConnectionString  = "server=localhost;uid=root;pwd=rootpassword;database=flightmanagement";
+                try
+                {
+                    conn = new MySqlConnection(myConnectionString);
+                    MySqlCommand cmd = new MySqlCommand("InsertFlightDetails", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Date", model.Date);
+                    cmd.Parameters.AddWithValue("@Time", model.Time);
+                    cmd.Parameters.AddWithValue("@SeatsAvailable", model.SeatsAvailable);
+                    cmd.Parameters.AddWithValue("@Runway", model.RunwayId);
+                    cmd.Parameters.AddWithValue("@Gate", model.Gate);
+                    cmd.Parameters.AddWithValue("@PlainId", model.PlaneID);
+                    cmd.Parameters.AddWithValue("@AirportID_Origin", model.DestinationId);
+                    cmd.Parameters.AddWithValue("@AirportID_Destination", model.OriginId);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+
+                }
+                catch (MySqlException ex)
+                {
+
+                }
+
+            return View("AddFlight", model);
+        }
+
+        public ActionResult AddPlane()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPlane(AddPlane mod)
+        {
+            MySqlConnection conn = null;
+            string myConnectionString = "server=localhost;uid=root;pwd=rootpassword;database=flightmanagement";
+            try
+            {
+                conn = new MySqlConnection(myConnectionString);
+                MySqlCommand cmd = new MySqlCommand("InsertPlaneDetails", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@PlaneID", mod.PlaneID);
+                cmd.Parameters.AddWithValue("@Model", mod.Model);
+                cmd.Parameters.AddWithValue("@Manufacturer", mod.Manufacturer);
+                cmd.Parameters.AddWithValue("@SeatCapacity", mod.SeatCapacity);
+                cmd.Parameters.AddWithValue("@Age", mod.Age);
+                cmd.Parameters.AddWithValue("@LastMaintenancedOn", mod.LastMaintenanceOn);
+                cmd.Parameters.AddWithValue("@NextMaintenancedOn", mod.NextMaintenanceOn);
+                cmd.Parameters.AddWithValue("@Maintenance_Comments", mod.Maintenance_Comments);
+                cmd.Parameters.AddWithValue("@EmployeeID_Maintenance", mod.EmployeeIdManatainace);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+
+            }
+
+            return View();
+        }
+
+        public ActionResult BookingFlights()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetFlights(string date)
+        {
+            MySqlConnection conn = null;
+            string myConnectionString = "server=localhost;uid=root;pwd=rootpassword;database=flightmanagement";
+            DataSet ds = new DataSet();
+            List<AddFlights> flight = new List<AddFlights>();
+            using ( conn = new MySqlConnection(myConnectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand("GetFlightDetails", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Date", date);
+                {
+                    cmd.Connection = conn;
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                    {
+                        sda.Fill(ds);
+                    }
+                }
+                foreach (DataRow dr in ds.Tables[0].Rows) // loop for adding add from dataset to list<modeldata>
+                {
+                    flight.Add(new AddFlights
+                    {
+                        FlightId = Convert.ToInt32(dr["FlightID"]),
+                        PlaneID = Convert.ToInt32(dr["PlaneID"]), // adding data from dataset row in to list<modeldata>
+                        Date = dr["Date"].ToString(),
+                        Time = dr["Time"].ToString(),
+                        SeatsAvailable = Convert.ToInt32(dr["SeatsAvailable"]),
+                        Price = Convert.ToInt32(dr["Price"]),
+                        OriginName = dr["OriginName"].ToString(),
+                        OriginCity = dr["OriginCity"].ToString(),
+                        OriginCountry = dr["OriginCountry"].ToString(),
+                        DestName = dr["DestName"].ToString(),
+                        DestCity = dr["DestCity"].ToString(),
+                        DestCountry = dr["DestCountry"].ToString()
+                    });
+                }
+            }
+            ViewBag.userdetails = flight;
+            return View("BookingFlights");
+        }
+
+        public ActionResult UpdateFlight(int flightid, int PlaneId, string Date, string Time, int SeatsAvailable, int Price)
+        {
+            AddFlights updateflight = new AddFlights();
+            ViewBag.flightid = flightid;
+            ViewBag.planeid = PlaneId;
+            ViewBag.Date = Date;
+            ViewBag.Time = Time;
+            ViewBag.seatsavailable = SeatsAvailable;
+            ViewBag.Price = Price;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UpdateFlight(AddFlights updateflight)
+        {
+            MySqlConnection conn = null;
+            string myConnectionString = "server=localhost;uid=root;pwd=rootpassword;database=flightmanagement";
+            using (conn = new MySqlConnection(myConnectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand("UpdateFlightDetails", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@p_FlightId", updateflight.FlightId);
+                cmd.Parameters.AddWithValue("@p_PlainId", updateflight.PlaneID);
+                cmd.Parameters.AddWithValue("@p_Date", updateflight.Date);
+                cmd.Parameters.AddWithValue("@p_Time", updateflight.Time);
+                cmd.Parameters.AddWithValue("@p_SeatsAvailable", updateflight.SeatsAvailable);
+                cmd.Parameters.AddWithValue("@p_Price", updateflight.Price);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return Redirect("GetFlights?date=" + updateflight.Date);
+            }
+        }
+
+        public ActionResult FlightBookingDetails(int flightid, int price)
+        {
+            Passenger psg = new Passenger();
+            psg.FlightID = flightid;
+            psg.AmountPaid = price;
+            return View(psg);
+        }
+
+        [HttpPost]
+        public ActionResult FlightBookingDetails(Passenger png)
+        {
+            MySqlConnection conn = null;
+            string myConnectionString = "server=localhost;uid=root;pwd=rootpassword;database=flightmanagement";
+            using (conn = new MySqlConnection(myConnectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand("SaveBooking", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@P_FirstName", png.FirstName);
+                cmd.Parameters.AddWithValue("@P_LastName", png.LastName);
+                cmd.Parameters.AddWithValue("@P_Address", png.Address);
+                cmd.Parameters.AddWithValue("@P_DOB", png.DOB);
+                cmd.Parameters.AddWithValue("@P_Email", png.EMail);
+                cmd.Parameters.AddWithValue("@P_PhoneNumber", png.PhoneNum);
+                cmd.Parameters.AddWithValue("@P_FlightID", png.FlightID);
+                cmd.Parameters.AddWithValue("@P_AmountPaid", png.AmountPaid);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            return View("BookingFlights");
+        }
+
+        public ActionResult AddAirport()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAirport(AddAirport Airport)
+        {
+            MySqlConnection conn = null;
+            string myConnectionString = "server=localhost;uid=root;pwd=rootpassword;database=flightmanagement";
+            using (conn = new MySqlConnection(myConnectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand("AddAirportDetails", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@P_AirportId", Airport.AirportId);
+                cmd.Parameters.AddWithValue("@P_Name", Airport.Name);
+                cmd.Parameters.AddWithValue("@P_City", Airport.City);
+                cmd.Parameters.AddWithValue("@P_Country", Airport.Country);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return View();
+            }
+        }
+
+        public ActionResult CheckIn()
+        {
+            return View("CheckInDetails");
+        }
+
+        [HttpGet]
+        public ActionResult CheckInDetails(int flightid, string Fname, string Lname)
+        {
+            MySqlConnection conn = null;
+            string myConnectionString = "server=localhost;uid=root;pwd=rootpassword;database=flightmanagement";
+            DataSet ds = new DataSet();
+            List<AddFlights> flights = new List<AddFlights>();
+            using (conn = new MySqlConnection(myConnectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand("GetCheckInInfo", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@P_FirstName", Fname);
+                cmd.Parameters.AddWithValue("@P_LastName", Lname);
+                cmd.Parameters.AddWithValue("@P_flightID", flightid);
+                {
+                    cmd.Connection = conn;
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter(cmd))
+                    {
+                        sda.Fill(ds);
+                    }
+                }
+                foreach (DataRow dr in ds.Tables[0].Rows) // loop for adding add from dataset to list<modeldata>
+                {
+                    flights.Add(new AddFlights
+                    {
+                        FlightId = Convert.ToInt32(dr["FlightID"]),
+                        PlaneID = Convert.ToInt32(dr["PlaneID"]), // adding data from dataset row in to list<modeldata>
+                        Date = dr["Date"].ToString(),
+                        Time = dr["Time"].ToString(),
+                        SeatsAvailable = Convert.ToInt32(dr["SeatsAvailable"]),
+                        OriginName = dr["OriginName"].ToString(),
+                        OriginCity = dr["OriginCity"].ToString(),
+                        OriginCountry = dr["OriginCountry"].ToString(),
+                        DestName = dr["DestName"].ToString(),
+                        DestCity = dr["DestCity"].ToString(),
+                        DestCountry = dr["DestCountry"].ToString(),
+                        IsCheckedIn = Convert.ToInt32(dr["IsCheckedIn"]),
+                        CustomerId = Convert.ToInt32(dr["CustomerId"])
+                    });
+                }
+            }
+            ViewBag.checkIndetails = flights;
+            return View();
+        }
+
+        public ActionResult SaveCheckIn(int flightid, int custid)
+        {
+            MySqlConnection conn = null;
+            string myConnectionString = "server=localhost;uid=root;pwd=rootpassword;database=flightmanagement";
+            using (conn = new MySqlConnection(myConnectionString))
+            {
+                MySqlCommand cmd = new MySqlCommand("SaveCheckIn", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@P_CheckIn", 1);
+                cmd.Parameters.AddWithValue("@P_custid", custid);
+                cmd.Parameters.AddWithValue("@P_flightid", flightid);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            return View("CheckInDetails");
+        }
+    }
+}
---
+++ Models/FlightsModels.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace FlightManagementSystem.Models
+{
+    public class FlightsModels
+    {
+
+    }
+
+    public class AddFlights
+    {
+        [Display(Name = "Customer ID")]
+        public int CustomerId { get; set; }
+
+        [Display(Name = "Flight ID")]
+        public int FlightId { get; set; }
+
+        [Required]
+        [Display(Name = "Flight Date")]
+        public string Date { get; set; }
+
+        [Required]
+        [Display(Name = "Flight Time")]
+        public string Time { get; set; }
+
+        [Required]
+        [Display(Name = "Seats Available")]
+        public int SeatsAvailable { get; set; }
+
+        [Display(Name = "Runway ID")]
+        public int RunwayId { get; set; }
+
+        [Display(Name = "Gate")]
+        public string Gate { get; set; }
+
+        [Required]
+        [Display(Name = "Plane ID")]
+        public int PlaneID { get; set; }
+
+        [Required]
+        [Display(Name = "Destination ID")]
+        public int DestinationId { get; set; }
+
+        [Required]
+        [Display(Name = "Origin ID")]
+        public int OriginId { get; set; }
+
+        [Display(Name = "Price")]
+        public int Price { get; set; }
+
+        [Display(Name = "Origin Name")]
+        public string OriginName { get; set; }
+
+        [Display(Name = "Origin City")]
+        public string OriginCity { get; set; }
+
+        [Display(Name = "Origin Country")]
+        public string OriginCountry { get; set; }
+
+        [Display(Name = "Destination Name")]
+        public string DestName { get; set; }
+
+        [Display(Name = "Destination City")]
+        public string DestCity { get; set; }
+
+        [Display(Name = "Destination Country")]
+        public string DestCountry { get; set; }
+
+        [Display(Name = "Is Checked In")]
+        public int IsCheckedIn { get; set; }
+    }
+
+    public class AddPlane
+    {
+        [Required]
+        [Display(Name = "Plane ID")]
+        public int PlaneID  { get; set; }
+
+        [Required]
+        [Display(Name = "Model")]
+        public string Model { get; set; }
+
+        [Required]
+        [Display(Name = "Manufacturer")]
+        public string Manufacturer { get; set; }
+
+        [Display(Name = "Seat Capacity")]
+        public int SeatCapacity { get; set; }
+
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        [Display(Name = "Last Maintenance On")]
+        public string LastMaintenanceOn { get; set; }
+
+        [Display(Name = "Next Maintenance On")]
+        public string NextMaintenanceOn { get; set; }
+
+        [Display(Name = "Maintenance Comments")]
+        public string Maintenance_Comments { get; set; }
+
+        [Display(Name = "Employee ID Manatainace")]
+        public int EmployeeIdManatainace { get; set; }
+    }
+   
+    public class Passenger
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string EMail { get; set; }
+
+        [Required]
+        [Display(Name = "Date of Birth")]
+        public string DOB { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNum { get; set; }
+
+        [Display(Name = "Seat Number")]
+        public string SeatNumber { get; set; }
+
+        [Display(Name = "Seat Class")]
+        public string SeatClass { get; set; }
+
+        [Required]
+        [Display(Name = "Amount Paid")]
+        public int AmountPaid { get; set; }
+
+        [Display(Name = "Meal Preference")]
+        public string MealPreference { get; set; }
+
+
+        [Display(Name = "Bag ID")]
+        public string BagID { get; set; }
+
+
+        [Display(Name = "Bag Weight")]
+        public string BagWeight { get; set; }
+
+        [Display(Name = "Is Checked In")]
+        public string IsCheckedIn { get; set; }
+
+        [Display(Name = "Flight ID")]
+        public int FlightID { get; set; }
+
+        [Display(Name = "Customer ID")]
+        public string CustomerID { get; set; }
+    }
+
+    public class AddAirport
+    {
+        [Required]
+        [Display(Name = "Airport ID")]
+        public int AirportId { get; set; }
+
+        [Required]
+        [Display(Name = "Airport Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Airport City")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Airport Country")]
+        public string Country { get; set; }
+    }
+
+    public enum FlightClass
+    {
+        EconomyClass,
+        PremiumEconomyClass,
+        BusinessClass,
+        FirstClass
+    }
+}
